c3372c31109eeb1f82ff1c17a6182c99352654e3
gustavosilveiragss <gustavo@pop-os.localdomain>
2023-06-09 00:06:01 -0300

    init cargo crate

---
+++ api/target/debug/deps/api-c75e574edac1adbd.d
@@ -0,0 +1,5 @@
+/home/gustavo/Documents/bin-monitor/api/target/debug/deps/api-c75e574edac1adbd.rmeta: src/main.rs
+
+/home/gustavo/Documents/bin-monitor/api/target/debug/deps/api-c75e574edac1adbd.d: src/main.rs
+
+src/main.rs:
